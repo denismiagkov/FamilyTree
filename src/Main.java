75693cff8eda86b3761cbd52dca6b146aa02862e
Denis Myagkov <tomich-84@mail.ru>
2023-04-02 17:16:55 +0300

    getChildren as method completed

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,30 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Human h1 = new Human("ego", "Денис", "Мягков", LocalDate.of(1984, 1, 22));
-        Human h2 = new Human("мать  ego", "Татьяна", "Мягкова", LocalDate.of(1958, 5, 16));
-        Human h3 = new Human("мать матери ego", "Галина", "Бородихина", LocalDate.of(1932, 1, 23));
-        Human h4 = new Human("отец матери ego", "Николай", "Мягков", LocalDate.of(1932, 12, 23));
-        Human h5 = new Human("сын матери матери ego: ", "Юрий", "Мягков", LocalDate.of(1954, 05, 8));
-        Human h6 = new Human("дочь матери матери ego: ", "Марина", "Мягкова", LocalDate.of(1964, 8, 5));
-        Human h7 = new Human("сын ego", "Матвей", "Мягков", LocalDate.of(2021, 10, 5));
-        Human h8 = new Human("дочь сына матери матери ego",  "Наталья", "Мягкова", LocalDate.of(1978, 9, 23));
-        Human h9 = new Human("дочь сына матери матери ego",  "Лариса", "Мягкова", LocalDate.of(1983, 8, 7));
-        Human h10 = new Human("дочь дочери матери матери ego",  "Юлия", "Констман", LocalDate.of(1988, 4, 12));
+        FamilyTree tree = new FamilyTree();
+        tree.addKin(new Human("мать матери ego", "Галина", "Бородихина", LocalDate.of(1932, 1, 23)));
+        tree.addKin(new Human("отец матери ego", "Николай", "Мягков", LocalDate.of(1932, 12, 23)));
+        tree.addKin(new Human("сын матери матери ego: ", "Юрий", "Мягков", LocalDate.of(1954, 05, 8), tree.getHuman("Галина", "Бородихина"), tree.getHuman("Николай", "Мягков")));
+        tree.addKin(new Human("мать  ego", "Татьяна", "Мягкова", LocalDate.of(1958, 5, 16), tree.getHuman("Галина", "Бородихина"), tree.getHuman("Николай", "Мягков")));
+        tree.addKin(new Human("дочь матери матери ego: ", "Марина", "Мягкова", LocalDate.of(1964, 8, 5), tree.getHuman("Галина", "Бородихина"), tree.getHuman("Николай", "Мягков")));
+        tree.addKin(new Human("ego", "Денис", "Мягков", LocalDate.of(1984, 1, 22), tree.getHuman("Татьяна", "Мягкова"), null));
+        tree.addKin(new Human("дочь сына матери матери ego", "Наталья", "Мягкова", LocalDate.of(1978, 9, 23), null, tree.getHuman("Юрий", "Мягков")));
+        tree.addKin(new Human("дочь сына матери матери ego", "Лариса", "Мягкова", LocalDate.of(1983, 8, 7), null, tree.getHuman("Юрий", "Мягков")));
+        tree.addKin(new Human("дочь дочери матери матери ego", "Юлия", "Констман", LocalDate.of(1988, 4, 12), tree.getHuman("Марина", "Мягкова"), null));
+        tree.addKin(new Human("сын ego", "Матвей", "Мягков", LocalDate.of(2021, 10, 5)));
 
-        h1.setMother(h2);
+        //Human h1 = new Human("ego", "Денис", "Мягков", LocalDate.of(1984, 1, 22));
+        //Human h2 = new Human("мать  ego", "Татьяна", "Мягкова", LocalDate.of(1958, 5, 16));
+        //Human h3 = new Human("мать матери ego", "Галина", "Бородихина", LocalDate.of(1932, 1, 23));
+        //Human h4 = new Human("отец матери ego", "Николай", "Мягков", LocalDate.of(1932, 12, 23));
+        //Human h5 = new Human("сын матери матери ego: ", "Юрий", "Мягков", LocalDate.of(1954, 05, 8));
+        //Human h6 = new Human("дочь матери матери ego: ", "Марина", "Мягкова", LocalDate.of(1964, 8, 5), tree.getHuman("Галина", "Бородихина"), tree.getHuman("Николай", "Мягков"));
+        //Human h7 = new Human("сын ego", "Матвей", "Мягков", LocalDate.of(2021, 10, 5));
+        //Human h8 = new Human("дочь сына матери матери ego", "Наталья", "Мягкова", LocalDate.of(1978, 9, 23));
+        //Human h9 = new Human("дочь сына матери матери ego", "Лариса", "Мягкова", LocalDate.of(1983, 8, 7));
+        //Human h10 = new Human("дочь дочери матери матери ego", "Юлия", "Констман", LocalDate.of(1988, 4, 12));
+
+        /*h1.setMother(tree.getHuman());
         h2.setMother(h3);
         h2.setFather(h4);
         h5.setMother(h3);
@@ -36,7 +48,7 @@ public class Main {
         tree.addKin(h7);
         tree.addKin(h8);
         tree.addKin(h9);
-        tree.addKin(h10);
+        tree.addKin(h10);*/
 
         // System.out.println(genTree);
 
@@ -45,11 +57,12 @@ public class Main {
         //System.out.println(h3.getChildren());
         //System.out.println(h5.getChildren());
 
-/*        System.out.println(tree.getOldest());
-        System.out.println(tree.getYoungest());
-        System.out.println(tree.getHuman("Денис", "Мягков"));
-        System.out.println(tree.getHuman("сын ego"));*/
+        //System.out.println(tree.getOldest());
+        //System.out.println(tree.getYoungest());
+        //System.out.println(tree.getHuman("Денис", "Мягков"));
+        System.out.println(tree.getHuman("сын ego"));
         System.out.println(tree.getChildren("Галина", "Бородихина"));
+
 
     }
 }
